7827f4897fcae84aad90c646efefd095cbbc91d2
Arkadiusz Chwedczuk <chwedczukarek@gmail.com>
2025-04-05 10:42:45 +0200

    chore: some clicking

---
+++ src/data/nav.ts
@@ -0,0 +1,8 @@
+
+
+export const navData = [
+  {href: "/xp", label: "Xp"},
+  {href: "/projects", label: "Projects"},
+  {href: "/about", label: "About"},
+  {href: "/contact", label: "Contact"},
+]
---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+
+
+export const dqsl = <T extends string>(selector: T) => {
+  const el = document.querySelector(selector)
+  if(el && el instanceof HTMLElement) return el
+};
+export const dqsla = <T extends string>(selector: T) => {
+  return Array.from(document.querySelectorAll(selector)).filter(el => el instanceof HTMLElement);
+}
+
