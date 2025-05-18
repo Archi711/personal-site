86061f3f3604fdbdeadab779854923c9417a2bc6
Arkadiusz Chwedczuk <chwedczukarek@gmail.com>
2025-05-18 14:57:05 +0200

    feat: home page word + Head + astro font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,31 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.DEV
+    ? "http://localhost:4321"
+    : "https://arekchwedcz.uk",
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [mdx()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Instrument Serif",
+        cssVariable: "--font-instrument-serif",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Atkinson Hyperlegible",
+        cssVariable: "--font-atkinson-hyperlegible",
+        weights: [400, 700],
+      },
+    ],
+  },
 });
---
+++ src/content/_unstructured/about.md
@@ -0,0 +1,12 @@
+# Hi, my name is Arek. I'm a fullstack dev and designer
+
+*i.e. I write a lot of TSX/Astro files and take care about it looks.*
+
+Hard to express my gratitude to see you here.
+My primary skills circle around TS and React. I'm fairly sure I can comprehend
+a lot - so that's why I'm also trying to make web look *good*.
+
+I'm known for making things work. Ugly landing is better that beautiful catastrophe.
+Uncle Bob would be mad, but in JS world, all sorts of heresy are accepted!
+
+*Anyway...*
